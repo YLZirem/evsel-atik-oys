1bc23f4c1a610a54d8279de4002964c3f7f9e236
YLZirem <irem03yilmaz@gmail.com>
2025-04-29 17:56:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-  e.preventDefault();  // Formun sayfayı yeniden yüklemesini engelle
-
-  // Kullanıcı adı ve şifre kontrolü (örnek: sabit bir değerle)
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-
-  // Basit bir kontrol - gerçekte veritabanı kullanmalısınız
-  if (username === "admin" && password === "1234") {
-    window.location.href = "index.html";  // Başarılı giriş sonrası yönlendirme
-  } else {
-    alert("Hatalı kullanıcı adı veya şifre!");
-  }
-});
-
-
 // Menü açma ve kapama
 function toggleMenu() {
   var sidebar = document.getElementById("sidebar");
